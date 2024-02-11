c81bc80d0bd162ccc17f56f775e4faa45a9a92a5
firasroggai <firasreggai@gmail.com>
2024-02-11 21:05:25 +0100

    Improved the art editor

--- src/lib/components/Heading.svelte
+++ src/lib/components/Heading.svelte
@@ -2,10 +2,6 @@
 export let text;
 </script>
 
-<<<<<<< HEAD
 <div class="border-b-2 border-black">
-=======
-<div class="border-2 rounded px-3 border-black">
->>>>>>> d667ae88886709eb1627c9cfda60627be930d189
     <h1 class="py-2 font-bold text-4xl">{text}</h1>
 </div>
--- src/routes/profile/[profile_id]/+page.svelte
+++ src/routes/profile/[profile_id]/+page.svelte
@@ -5,16 +5,11 @@
 
     export let data;
 
-<<<<<<< HEAD
     const { display_name, user_id, coins, palettes, profile_pic_id, role } =
-=======
-    const { display_name, user_id, coins, palletes, profile_pic_id, role } =
->>>>>>> d667ae88886709eb1627c9cfda60627be930d189
         data;
 </script>
 
 <Card.Root class="border border-black w-[300px]">
-<<<<<<< HEAD
     <ArtDisplay pixel_data={profile_pic_id} />
     <Card.Content class="p-2">
         <Card.Title class="pb-1">Name: {display_name}</Card.Title>
@@ -24,16 +19,6 @@
             <p>
                 Available palettes :
                 {palettes.join(",")}
-=======
-    <ArtDisplay pixel_data={data.profile_pic_id} />
-    <Card.Content class="p-2">
-        <Card.Title>Name: {display_name}</Card.Title>
-        <Card.Description>ID: @{user_id}</Card.Description>
-        <Card.Description>
-            <p>
-                Available palettes :
-                {data.palettes.join(",")}
->>>>>>> d667ae88886709eb1627c9cfda60627be930d189
             </p>
             <p>Coins: {coins}</p>
         </Card.Description>
